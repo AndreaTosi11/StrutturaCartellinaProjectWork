3ca4043c9c2f2db91a6df0c4f600d059df17eab7
AndreTo11 <andrea.tosi11@yahoo.com>
2022-06-21 20:06:06 +0200

    a

--- WebApp-Cliente/frontEnd/angular/WebAppClientProject/src/app/model/banca-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BancaModel } from './banca-model';
-
-describe('BancaModel', () => {
-  it('should create an instance', () => {
-    expect(new BancaModel()).toBeTruthy();
-  });
-});
--- WebApp-Cliente/frontEnd/angular/WebAppClientProject/src/app/service/banca-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BancaService } from './banca-service';
-
-describe('BancaService', () => {
-  it('should create an instance', () => {
-    expect(new BancaService()).toBeTruthy();
-  });
-});
